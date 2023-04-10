5b873160a166b118989c8a32dfa34b44fb4dfff8
cwsprandel <110702891+cwsprandel@users.noreply.github.com>
2023-04-10 09:45:46 -0500

    Using Javascript and Webpack for Page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-alert("Hello, World!")
+function render() {
+    const webContent = document.getElementById("content");
+
+    const webHeader = document.createElement("div");
+    webHeader.setAttribute("class", "header");
+
+    const headerTitle = document.createElement("h1");
+    headerTitle.textContent = "This is the Restaurant Header";
+
+    webHeader.appendChild(headerTitle);
+
+    const webSidebar = document.createElement("div");
+    webSidebar.setAttribute("class", "sidebar");
+
+    const sidebarList = document.createElement("ul");
+    sidebarList.setAttribute("class", "sidebar");
+
+    const listItemOne = document.createElement("li");
+    listItemOne.textContent = "Stuff";
+    const listItemTwo = document.createElement("li");
+    listItemTwo.textContent = "More Stuff";
+    const listItemThree = document.createElement("li");
+    listItemThree.textContent = "Even More Stuff";
+
+    sidebarList.appendChild(listItemOne);
+    sidebarList.appendChild(listItemTwo);
+    sidebarList.appendChild(listItemThree);
+
+    webContent.appendChild(webHeader);
+    webContent.appendChild(sidebarList);
+
+    return {
+        webContent
+    }
+}
+
+document.body.appendChild(render());
